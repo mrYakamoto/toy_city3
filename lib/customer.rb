75144d51e2a13a69a7ca4c6e66178ca4df410895
Gregory Wehmeier <wehmenstein@gmail.com>
2016-03-03 10:41:31 -0800

    use class variables for #all methods. refactor a couple methods

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,10 @@
 class Customer
+  @@all_customers = []
+
   def initialize(options = {})
     validates_name_uniqueness(options.fetch(:name))
     @name = options.fetch(:name)
+    @@all_customers << self
   end
 
   def name
@@ -9,6 +12,7 @@ class Customer
   end
 
   def purchase(product)
+    raise OutOfStockError, "'#{product.title}' is out of stock" if product.stock === 0
     Transaction.new(self, product)
   end
 
@@ -23,21 +27,17 @@ class Customer
   end
 
   def self.all
-    all_customers = []
-    ObjectSpace.each_object(Customer) do |customer|
-      all_customers << customer
-    end
-    all_customers
+    @@all_customers
   end
 
   def self.find_by_name(name)
-    Customer.all.each do |customer|
+    @@all_customers.each do |customer|
       return customer if customer.name == name
     end
   end
 
   def validates_name_uniqueness(name)
-    Customer.all.each do |existing_customer|
+    @@all_customers.each do |existing_customer|
       raise DuplicateCustomerError, "'#{name}' already exists" if existing_customer.name == name
     end
     true
